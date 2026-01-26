58293ec1fe56d09707e30b8caf1eb9c576bbae69
Losker <loskertopgame@gmail.com>
2026-01-26 21:53:42 +0200

    object

--- Stroki i drugie/src/Pain.java
+++ Stroki i drugie/src/Pain.java
@@ -1,30 +1,23 @@
 public class Pain {
-    public static int sum(int[] numbers) {
-        int sum = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            sum = sum + numbers[i];
-        }
-        return sum;
-    }
-    public static void printIssues(int issueCount) {
-        System.out.println(issueCount);
 
-    }
-    public static void printSeparator() {
-        System.out.println("=========+_+==========");
-        System.out.println("----------------------");
-    }
     public static void main(String[] args) {
-        int[] issuesByMonths = {4, 6, 7, 9, 2, 5, 12, 3, 7, 10, 6, 7, 1, 8};
-        printSeparator();
-        for (int i = 0; i < issuesByMonths.length; i++) {
-            printIssues(issuesByMonths[i]);
-            if ((i + 1) % 3 == 0) {
-                printSeparator();
-            }
+        String nameJohn = "John";
+        int age = 13;
+
+        String nameSarah = "Sarah";
+        int ageSarah = 30;
+
+        String[] names = {"John", "Sarah"};
+        int[] ages = {13, 30};
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println("Имя - " + names[i] + " Возраст - " + ages[i]);
         }
-        printSeparator();
-        int total = sum(issuesByMonths);
-        printIssues(total);
+        Person sarah = new Person("Sarah", 30);
+        System.out.println("sarah.name = " + sarah.name);
+        System.out.println("sarah.age = " + sarah.age);
+        Person john = new Person("John", 13);
+        System.out.println("sarah.name = " + john.name);
+        System.out.println("sarah.age = " + john.age);
     }
 }
