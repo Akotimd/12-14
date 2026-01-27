a4b530d41741547bec9dee4d05d16db0587107ac
Losker <loskertopgame@gmail.com>
2026-01-28 00:14:21 +0200

    hw14

--- book/src/App.java
+++ book/src/App.java
@@ -1,5 +1,16 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-
+    public static void main(String[] args) {
+        Author georgeOrwell =  new Author("Дожрдж", "Оруэлл");
+        Author duglasAdams = new Author("Дуглас", "Адамс");
+        Book oneNineFourEight = new Book("1984", georgeOrwell, 1948);
+        Book theHitchhikerGuideToTheGalaxy = new Book("Автостопом по Галактике", duglasAdams, 1979);
+        System.out.println("Название - " + oneNineFourEight.getTitle());
+        System.out.println("Автор - " + oneNineFourEight.getAuthor());
+        System.out.println("Год издания - " + oneNineFourEight.getYear());
+        System.out.println("Название - " + theHitchhikerGuideToTheGalaxy.getTitle());
+        System.out.println("Автор - " + theHitchhikerGuideToTheGalaxy.getAuthor());
+        System.out.println("Год издания - " + theHitchhikerGuideToTheGalaxy.getYear());
+        theHitchhikerGuideToTheGalaxy.setYear(1900);
+        System.out.println("Изменёный год создания - " + theHitchhikerGuideToTheGalaxy.getYear());
     }
 }
--- book/src/Book.java
+++ book/src/Book.java
@@ -1,5 +1,20 @@
 public class Book {
     private String title;
-    private String author;
+    private Author author;
     private int year;
+
+    public Book(String title, Author author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+
+    }
+    public String getAuthor() {
+        return this.author.getFullName();
+    }
+    public String getTitle() {
+        return this.title;
+    }
+    public int getYear() {return this.year;}
+    public void setYear(int year) {this.year = year;}
 }
